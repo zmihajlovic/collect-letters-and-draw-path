701cf88ede7823ae5f5ef20edaa2f542fa509806
Zeljko Mihajlovic <z.mihajlovic2307@gmail.com>
2025-04-26 00:17:09 +0200

    change type

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -3,6 +3,6 @@
  * @param prop error as Error
  * @returns error ui
  */
-export const ErrorFallback = ({ error }: { error: Error }) => {
+export const ErrorFallback = ({ error }: { error: number }) => {
   return <p>{error.message}</p>;
 };
